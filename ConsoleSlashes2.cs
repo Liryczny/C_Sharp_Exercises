17cbc10a04d3ff3ec51023671405d40c17897d96
Liryczny <wrzosek95@o2.pl>
2016-02-24 09:24:13 +0000

    Console Slashes 2
    Using:
    Slashes,
    Color of Console

---
+++ ConsoleSlashes2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+ public class ConsoleSlashes2
+ {
+   public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("          ");
+            Console.WriteLine("  \\\\\\\\    ");
+            Console.WriteLine(" /-----\\  ");
+            Console.WriteLine("[|O   O|] ");
+            Console.WriteLine(" |  |  |  ");
+            Console.WriteLine(" | ___ |  ");
+            Console.WriteLine("  \\___/   ");
+            Console.WriteLine("          ");
+            Console.ResetColor();
+        }
+  }
+
