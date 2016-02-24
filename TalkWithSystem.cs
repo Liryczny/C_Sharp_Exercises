8729dee706b55b6bdf9b0efa96e3746f79110f24
Liryczny <wrzosek95@o2.pl>
2016-02-24 10:23:51 +0000

    Talk With System
    Using:
    Writing into console
    Reading line from console
    if statements

---
+++ TalkWithSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class TalkWithSystem
+{
+  public static void Main(string[] args)
+  {
+     string response = "";
+     Console.WriteLine("SYSTEM: What's up");
+     Console.Write("USER: ");
+     response = Console.ReadLine().ToUpper();
+
+     if (response == "GOOD")
+     {
+       Console.WriteLine("SYSTEM: That's good");
+     }
+     else
+     {
+       Console.WriteLine("SYSTEM: That's bad");
+     }
+  }
+}
+
