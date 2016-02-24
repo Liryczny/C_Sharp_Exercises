e1221242f6248cec9b5e99a548e223b05aae06a6
Liryczny <wrzosek95@o2.pl>
2016-02-24 09:14:32 +0000

    Console Slashes
    Practicing using slashes in Console.

---
+++ ConsoleSlashes.cs
@@ -0,0 +1,12 @@
+using System;
+public class ConsoleSlashes
+{
+   public static void Main(string[] args)
+   {
+      Console.WriteLine("          ___          ");
+      Console.WriteLine("|        /   \\   |     ");
+      Console.WriteLine("|       |     |  |     ");
+      Console.WriteLine("|       |     |  |     ");
+      Console.WriteLine("|_____   \\___/   |_____");
+   }
+}
